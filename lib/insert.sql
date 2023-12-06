e2ff2725030807430c5e2ab11406528dd510f64e
mararo-ops <danielmararo1@gmail.com>
2023-12-06 23:22:39 +0300

    complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperature,alive) VALUES ( 'Mr.Chocolate', 49, 'male', 'pale', 'optimum', 'yes');
+INSERT INTO bears (name,age,sex,color,temperature,alive) VALUES ('Rowdy' 19, 'male', 'white', 'normal', 'yes');
+INSERT INTO bears (name,age,sex,color,temperature,alive) VALUES ('Tabitha',32, 'female', 'black','optimum', 'yes');
+INSERT INTO bears (name,age,sex,color,temperature,alive) VALUES ('sergeant Brown', 970, 'null', 'below optimum', 'not sure');
+INSERT INTO bears (name,age,sex,color,temperature,alive) VALUES ('Melisa',16,'female', 'Asian', 'perfect', 'yes');
+INSERT INTO bears (name,age,sex,color,temperature,alive) VALUES ('Grinch',44, 'hybrid', 'pangea', 'excellent', 'yes');
+INSERT INTO bears (name,age,sex,color,temperature,alive) VALUES ('wendy',23, 'female', 'red indian', 'good', 'yes');
+INSERT INTO bears (name,age,sex,color,temperature,alive) VALUES ('null', 28, 'null', 'null', 'good', 'yes');
